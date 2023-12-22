152816201b90d8a29f46532a1df393693c5bda8f
imran0dot <imran0dot@gmail.com>
2023-12-22 10:38:37 +0600

    connect with github

---
+++ components/Title.tsx
@@ -0,0 +1,9 @@
+import React, { ReactNode } from 'react';
+
+const Title = ({ children }: { children: ReactNode }) => {
+    return (
+        <h2>{children}</h2>
+    );
+};
+
+export default Title;
